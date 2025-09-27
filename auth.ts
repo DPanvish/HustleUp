83d0db050456e78ae7f841e37b4f38f536b0cfe2
DOWRIPILLI PANVISH <119529390+DPanvish@users.noreply.github.com>
2025-09-27 11:19:51 +0530

    Google and Github Auth Added

--- auth.ts
+++ auth.ts
@@ -3,5 +3,14 @@ import GitHub  from "next-auth/providers/github"
 import Google from "next-auth/providers/google"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [GitHub, Google],
+  providers: [
+    GitHub({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
+    }),
+    Google({
+      clientId: process.env.AUTH_GOOGLE_CLIENT_ID,
+      clientSecret: process.env.AUTH_GOOGLE_CLIENT_SECRET,
+    }),
+  ],
 })
