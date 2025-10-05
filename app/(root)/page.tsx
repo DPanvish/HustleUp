2bd40b4b37bd1bffecfaa397840abf3b161e3276
DOWRIPILLI PANVISH <119529390+DPanvish@users.noreply.github.com>
2025-10-05 22:08:12 +0530

    Hultles and Startups Section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -11,12 +11,18 @@ export default async function Home({searchParams}: {searchParams: Promise<{query
 
   return (
     <>
-      <section className="pink_container">
+      <section className="pink-container">
         <h1 className="heading">Pitch Your Startup, <br /> Connect with Entrepreneurs</h1>
 
         <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.</p>
 
         <SearchBar query={query}/>
+      </section>
+
+      <section className="section-container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
       </section>
     </>
   );
