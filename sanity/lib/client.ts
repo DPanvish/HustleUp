52cd66d371a364ab125b3c6d91e1ddffd552dcb5
DOWRIPILLI PANVISH <119529390+DPanvish@users.noreply.github.com>
2025-10-17 22:15:50 +0530

    client

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -6,5 +6,8 @@ export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  useCdn: false, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
+
+// Setting useCdn to `false` is important for preview mode
+// It will make sure the data is fetched fresh every time it's accessed.
