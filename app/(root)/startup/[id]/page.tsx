a2d1acb13336c7c6b7477ae051c1b41fdd6d8894
DOWRIPILLI PANVISH <119529390+DPanvish@users.noreply.github.com>
2025-11-09 22:36:13 +0530

    Editor picks playlist added

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { formatDate } from '@/lib/utils';
 import { client } from '@/sanity/lib/client';
-import { STARTUP_BY_ID_QUERY } from '@/sanity/lib/queries';
+import { PLAYLIST_BY_SLUG_QUERY, STARTUP_BY_ID_QUERY } from '@/sanity/lib/queries';
 import Image from 'next/image';
 import Link from 'next/link';
 import { notFound } from 'next/navigation';
@@ -8,6 +8,7 @@ import React, { Suspense } from 'react'
 import markdownit from "markdown-it";
 import { Skeleton } from '@/components/ui/skeleton';
 import View from '@/components/View';
+import StartupCard, { StartupCardType } from '@/components/StartupCard';
 
 
 const md = markdownit();
@@ -17,7 +18,8 @@ const page = async ({params}: {params: Promise<{id:string}>}) => {
   const id = (await params).id;
 
   const post = await client.fetch(STARTUP_BY_ID_QUERY, {id});
-  console.log(post);
+  
+  const {select: editorPosts} = await client.fetch(PLAYLIST_BY_SLUG_QUERY, {slug: "editor-picks-new"});
 
   if(!post){
       return notFound();
@@ -61,6 +63,18 @@ const page = async ({params}: {params: Promise<{id:string}>}) => {
           </div>
 
           <hr className="divider" />
+
+          {editorPosts?.length > 0 && (
+            <div className="max-w-4xl mx-auto">
+              <p className="text-30-semibold">Editor Picks</p>
+
+              <ul className="mt-7 card-grid-sm">
+                {editorPosts.map((post: StartupCardType, i: number) => (
+                  <StartupCard key={i} post={post} />
+                ))}
+              </ul>
+            </div>
+          )}
           
           {/* 
               This is uses nextjs PPR to render the view component 
